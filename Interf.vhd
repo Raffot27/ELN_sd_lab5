db53d2e10355f44bc92a6e743a8353b31114da3b
CrazyBJT96 <38176829+CrazyBJT96@users.noreply.github.com>
2018-04-21 19:27:45 +0200

    Interfaccia parte 4

---
+++ Interf.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Interf is
+port (CLOCK_50 : in std_logic;
+      Key : in std_logic_vector(0 downto 0);
+		Hex0, Hex1, Hex2, Hex3, Hex4, Hex5, Hex6, Hex7 : OUT std_logic_vector(6 downto 0)
+      );
+end Interf;
+
+architecture struct of Interf is
+component Hello_FSM 
+port( clk, rst : in std_logic;
+		Hex0, Hex1, Hex2, Hex3, Hex4, Hex5, Hex6, Hex7 : OUT std_logic_vector(6 downto 0)
+		);
+end component;
+begin 
+
+FSM : Hello_FSM port map (CLOCK_50, Key(0), Hex0, Hex1, Hex2, Hex3, Hex4, Hex5, Hex6, Hex7);
+end struct;
