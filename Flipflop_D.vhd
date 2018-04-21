c4f28201b85be16e22c2c16f4eb0c7fb73476ccc
CrazyBJT96 <38176829+CrazyBJT96@users.noreply.github.com>
2018-04-21 18:07:02 +0200

    Update Flipflop_D.vhd

--- Flipflop_D.vhd
+++ Flipflop_D.vhd
@@ -2,16 +2,17 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity D_flipflop is
-    port (D, clk, Set, reset: in std_logic;
-        Q: out std_logic);
+    port (  D, clk, Set, reset: in std_logic;
+        Q: out std_logic;
+		   f : out std_logic);
 end D_flipflop;
 
 architecture behavior of D_flipflop is
-signal temp : std_logic;
+signal cnt : std_logic := '0';
 begin
 
 process(clk)
- 
+
 begin
      if (clk'event and clk = '1') then
 		  if (reset = '0') then
@@ -20,11 +21,11 @@ begin
 		  Q <= not(D);
 		  else
 		  Q <= D;
-		  
+		  cnt <= '1';
 		  end if;
 		  end if;
          
 		  end process;
 		  --temp <= D;
-
+f <= cnt;
 end behavior;
